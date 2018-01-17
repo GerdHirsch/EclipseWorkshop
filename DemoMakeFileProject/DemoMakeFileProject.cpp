81152040381f1857e3173f6bb81d2636d2ad8baf
GerdHirsch <kontakt@gerdhirsch.de>
2018-01-17 19:47:43 +0100

    First commit

---
+++ DemoMakeFileProject/DemoMakeFileProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DemoMakeFileProject.cpp
+// Author      : Gerd Hirsch
+// Version     :
+// Copyright   : All rights reserved
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Make");
+	return EXIT_SUCCESS;
+}
