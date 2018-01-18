5ddcab6b0d3f193bfd470827b49a48aea9a9ee91
GerdHirsch <kontakt@gerdhirsch.de>
2018-01-18 17:03:23 +0100

    DLLClient & DynamicLib Demo

---
+++ DLLClient/setPath.bat
@@ -0,0 +1,1 @@
+set PATH=%PATH%;C:\Users\Gerd\DevProjects\Cpp\VSEclipseWorkshop\DynamicLib\Debug
---
+++ DynamicLib/DLLClass.cpp
@@ -0,0 +1,23 @@
+/*
+ * DLLClass.cpp
+ *
+ *  Created on: 31.03.2014
+ *      Author: Gerd
+ */
+
+#include "DLLClass.h"
+#include <iostream>
+using namespace std;
+
+DLLClass::DLLClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DLLClass::~DLLClass() {
+	// TODO Auto-generated destructor stub
+}
+
+void DLLClass::operation() {
+	cout << "Hello DLL" << endl;
+}
