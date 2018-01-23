b569ba72cec3f8239391ce0eff8c372e5736f3ff
GerdHirsch <kontakt@gerdhirsch.de>
2018-01-23 16:04:02 +0100

    Probleme mit DLL Pfad und Git in RunConfiguration

--- DLL/DLLClient/main.cpp
+++ DLL/DLLClient/main.cpp
@@ -12,6 +12,8 @@ int main(){
 	cout << "begin DLLClient" << endl;
 
 	cout << "PATH:" << getenv("PATH") << endl;
+	cout << "DLLPath:" << getenv("DLLPath") << endl;
+	cout << "git_work_tree:" << getenv("git_work_tree") << endl;
 //	cout << "BATH:" << getenv("BATH") << endl;
 
 	DLLClass o;
